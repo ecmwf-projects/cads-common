c83392112129a9c9f07f7f21213f953c1e7a4058
Concetta <c.tosto@bopen.eu>
2023-04-12 11:29:13 +0200

    fix processors

--- cads_common/logging.py
+++ cads_common/logging.py
@@ -1,9 +1,19 @@
 import structlog
 
 
-def logging_config(config_processors: list):
+def config_logging(additional_processors: list):
     structlog.configure(
-        processors=config_processors,
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer(),
+            *additional_processors,
+        ],
         wrapper_class=structlog.stdlib.BoundLogger,
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
